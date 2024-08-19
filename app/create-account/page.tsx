759985887dffeb89cf7e62433c1d4f6da78392b5
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-08-19 22:56:32 +0900

    액션의 유효성 검사를 위해 Zod 도입

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,41 +1,37 @@
+"use client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
+import { createAccount } from "./actions";
+import { useFormState } from "react-dom";
 
 export default function CreateAccount() {
+  const [state, action] = useFormState(createAccount, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl ">안녕하세요!</h1>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form action={action} className="flex flex-col gap-3">
         <FormInput
           name="username"
           required
           type="text"
           placeholder="Username"
-          errors={[]}
         />
-        <FormInput
-          name="email"
-          required
-          type="email"
-          placeholder="Email"
-          errors={[]}
-        />
+        <FormInput name="email" required type="email" placeholder="Email" />
         <FormInput
           name="password"
           required
           type="password"
           placeholder="Password"
-          errors={[]}
         />
         <FormInput
-          name="password-confirm"
+          name="confirm_password"
           required
           type="password"
           placeholder="Confirm Password"
-          errors={[]}
         />
         <FormButton text="Create Account" />
       </form>
