5d38eafe1603648646d6b7c1c3976e9aba60072f
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-08-20 17:01:44 +0900

    Refinement
    Zod에는 문자열 스키마를 만들 때, 몇 가지 오류 메시지를 지정할 수 있다.
    
    ex)
    const name = z.string({
        required_error: "Name은 필수입니다.",
        invalid_type_error:"Name은 문자열이어야 합니다.",
    });
    
    유효성 검사 메서드를 사용할 때 추가 인수를 전달해서 사용자 지정 오류 메시지를 제공할 수 있다.
    
    ex) z.string().min(5, { message : "5글자 이상 되어야 합니다."});
    
    추가로, .refine 메서드를 통해 사용자 지정 유효성 검사를 할 수 있다.
    
    .refine은 2개의 인수를 받는다.
    1. 유효성 검사 함수
    2. 몇 가지 옵션
    - message : 에러 메시지 지정
    - path : 에러 경로 지정
    - params : 에러 시 메시지를 커스텀하기 위해 사용하는 객체

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,38 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+function checkUsername(username: string) {
+  return !username.includes("user");
+}
+
+function checkPassword({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) {
+  return password === confirm_password ? true : false;
+}
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a String!",
+        required_error: "Please write a Username!",
+      })
+      .min(3, "Way too short!")
+      .max(10, "That is too long!")
+      .refine(checkUsername, 'No "user" allowed!'),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  .refine(checkPassword, {
+    message: "Both passwords should be the same!",
+    path: ["confirm_password"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
